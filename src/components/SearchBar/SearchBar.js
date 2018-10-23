ee40fd1c40a0584d8001ef2ca7310ded93ca9210
Finley Williamson <finley.williamson@gmail.com>
2018-10-23 17:03:51 -0400

    finish new theme

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -16,15 +16,17 @@ class SearchBar extends React.Component {
     return (
       <div id='search-bar-div' className='container-fluid'>
         <div id='search-bar-row' className="row d-flex align-items-center">
-          <div id='title-col' className="col-12 col-md-6">
+          <div id ='hidden-col-pre' className="col d-none d-md-block"></div>
+          <div id='title-col' className="col-12 col-md-5">
             <Title />
           </div>
-          <div id='address-input-col' className="col-12 col-md-6">
+          <div id='address-input-col' className="col-12 col-md-5">
             <AddressInput
               setAddressValue={this.props.setAddressValue}
               fetchLocation={this.props.fetchLocation}
             />
           </div>
+          <div id ='hidden-col-post' className="col d-none d-md-block"></div>
         </div>
       </div>
     );
